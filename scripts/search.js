31aaff00bb12304c27602db992a95df8212797d1
alejahenaoes <76217198+alejahenaoes@users.noreply.github.com>
2021-03-18 12:54:45 -0500

    autocomplete en array listo

--- scripts/search.js
+++ scripts/search.js
@@ -1,7 +1,14 @@
-// const apiKey = "y9Vum3HsDR775RvR5yVqbdDQ155aMeRX";
+//const apiKey = "api_key=y9Vum3HsDR775RvR5yVqbdDQ155aMeRX";
 function sendApiRequest(){
     let userInput = document.getElementById("input").value
     console.log(userInput)
+    //Autocompletar
+    let q = `q=${userInput}`;
+    let Autocomplete = "https://api.giphy.com/v1/gifs/search/tags?"+q+"?&"+apiKey;
+    fetch(Autocomplete)
+    .then(response => response.json())
+    .then(data => console.log(data)) //Array con datos
+    //resultados
     const searchApiURL = `https://api.giphy.com/v1/gifs/search?q=${userInput}&rating=g&${apiKey}`
     fetch(searchApiURL).then(function(data){
         return data.json()
@@ -19,4 +26,15 @@ function sendApiRequest(){
                     alert("Busqueda inválida")
             })
 }
-const searchContainer = document.getElementsByClassName("searchResults");
+//Poder buscar con enter
+let searchBar = document.getElementsByClassName("input")
+searchBar[0].addEventListener('keydown', (event) => {
+    if (event.keyCode === 13) {
+      sendApiRequest()
+    }
+  });
+
+const sugerencias = document.getElementsByClassName("sugerencias");
+
+let searchInput = document.getElementsByClassName("input")[0];
+
