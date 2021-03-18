bc07b5ad3f53f7fe52f66c0eefe5c345672c18c2
alejahenaoes <76217198+alejahenaoes@users.noreply.github.com>
2021-03-18 13:59:00 -0500

    funcionalidad active searchBar

--- scripts/search.js
+++ scripts/search.js
@@ -28,13 +28,30 @@ function sendApiRequest(){
 }
 //Poder buscar con enter
 let searchBar = document.getElementsByClassName("input")
+let activeSection = document.getElementsByClassName("active")
+let normalSection = document.getElementsByClassName("iconSearch")
 searchBar[0].addEventListener('keydown', (event) => {
     if (event.keyCode === 13) {
       sendApiRequest()
     }
   });
+searchBar[0].addEventListener("focus", () =>{
+    activeSection[0].style.display = "flex";
+    normalSection[0].style.display = "none";
+
+})
 
 const sugerencias = document.getElementsByClassName("sugerencias");
 
 let searchInput = document.getElementsByClassName("input")[0];
 
+// searchSinContenido.classList.add('activeSection');
+//     trendingSection.classList.remove('desactiveSection');
+
+// search_input[0].addEventListener("focus", () =>{
+//     search_icon_active[0].style.display = "block";
+//     search_box[0].style.borderBottom =  "1px solid #9CAFC3";
+//     search_icon[0].style.display = "none";
+//     close_suggest_icon.style.display = "block";
+//     suggest_box[0].style.display = "block";
+// });
