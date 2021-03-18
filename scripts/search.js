1b9208a18d3840a6f5488362cf5138e3c40a6bd4
alejahenaoes <76217198+alejahenaoes@users.noreply.github.com>
2021-03-18 18:34:45 -0500

    agregar limpieza y cerrar searchBar

--- scripts/search.js
+++ scripts/search.js
@@ -30,28 +30,23 @@ function sendApiRequest(){
 let searchBar = document.getElementsByClassName("input")
 let activeSection = document.getElementsByClassName("active")
 let normalSection = document.getElementsByClassName("iconSearch")
+let closeIcon = document.getElementsByClassName("closeSearch")
 searchBar[0].addEventListener('keydown', (event) => {
     if (event.keyCode === 13) {
       sendApiRequest()
     }
   });
+  //Funcionalidad de barra activa
 searchBar[0].addEventListener("focus", () =>{
     activeSection[0].style.display = "flex";
     normalSection[0].style.display = "none";
-
+})
+closeIcon[0].addEventListener("focus", () =>{
+    activeSection[0].style.display = "none";
+    normalSection[0].style.display = "block";
+    let inputValue = document.getElementById("input").value = "";
 })
 
 const sugerencias = document.getElementsByClassName("sugerencias");
 
 let searchInput = document.getElementsByClassName("input")[0];
-
-// searchSinContenido.classList.add('activeSection');
-//     trendingSection.classList.remove('desactiveSection');
-
-// search_input[0].addEventListener("focus", () =>{
-//     search_icon_active[0].style.display = "block";
-//     search_box[0].style.borderBottom =  "1px solid #9CAFC3";
-//     search_icon[0].style.display = "none";
-//     close_suggest_icon.style.display = "block";
-//     suggest_box[0].style.display = "block";
-// });
